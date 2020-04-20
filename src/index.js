88fac123bc5c75d0e9b46f4b6394f9b41dd31b35
Larissa Araújo <lariaraujo201@gmail.com>
2020-04-20 10:37:17 -0300

    feat: add function isFibonacci

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 'use strict'
 
-const fibonacci = () => null
+const posFibonacci = (num) => {
+    if (num <= 1) return num;
 
-const isFibonnaci = (num) => null
+    return posFibonacci(num - 1) + posFibonacci(num - 2);
+}
+
+const fibonacci = () => {
+    const sequence = [];
+
+    for (let num = 0; num <= 14; num++) { 
+       let result = posFibonacci(num);
+
+       sequence.push(result);
+    }
+    
+    return sequence;
+}
+
+const isFibonnaci = (num) => {
+    const sequence = fibonacci();
+
+    const is = sequence.find(number => number === num);
+
+    if (!is) return false
+
+    return true;
+}
+
+// const result = fibonacci();
+// console.log(result);
+
+// const result2 = isFibonnaci(0);
+// console.log(result2);
 
 module.exports = {
     fibonacci,
